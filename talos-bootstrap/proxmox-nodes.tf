a643166a90fc55fb90e2206420c2c3ce05c0af93
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2025-07-19 17:40:47 -0400

    Fix images

--- talos-bootstrap/proxmox-nodes.tf
+++ talos-bootstrap/proxmox-nodes.tf
@@ -9,7 +9,7 @@ locals {
         memory       = 16
         disk         = 100
         arch         = "amd64"
-        image = "https://factory.talos.dev/image/58e4656b31857557c8bad0585e1b2ee53f7446f4218f3fae486aa26d4f6470d8/v1.9.2/nocloud-amd64.raw.zst"
+        image        = "https://factory.talos.dev/image/58e4656b31857557c8bad0585e1b2ee53f7446f4218f3fae486aa26d4f6470d8/v1.9.2/nocloud-amd64.raw.zst"
       },
       {
         name         = "controlplane-2"
@@ -20,7 +20,7 @@ locals {
         memory       = 16
         disk         = 100
         arch         = "amd64"
-        image = "https://factory.talos.dev/image/58e4656b31857557c8bad0585e1b2ee53f7446f4218f3fae486aa26d4f6470d8/v1.9.2/nocloud-amd64.raw.zst"
+        image        = "https://factory.talos.dev/image/58e4656b31857557c8bad0585e1b2ee53f7446f4218f3fae486aa26d4f6470d8/v1.9.2/nocloud-amd64.raw.zst"
       },
       {
         name         = "controlplane-3"
@@ -31,27 +31,29 @@ locals {
         memory       = 16
         disk         = 100
         arch         = "amd64"
-        image = "https://factory.talos.dev/image/58e4656b31857557c8bad0585e1b2ee53f7446f4218f3fae486aa26d4f6470d8/v1.9.2/nocloud-amd64.raw.zst"
+        image        = "https://factory.talos.dev/image/58e4656b31857557c8bad0585e1b2ee53f7446f4218f3fae486aa26d4f6470d8/v1.9.2/nocloud-amd64.raw.zst"
       },
-    #   {
-    #     name         = "worker-1"
-    #     pve_node     = "atuan"
-    #     controlplane = false
-    #     ip           = "10.0.0.51"
-    #     cpu          = 12
-    #     memory       = 16
-    #     disk         = 100
-    #     arch         = "amd64"
-    #   },
-    #   {
-    #     name         = "worker-2"
-    #     pve_node     = "atuan"
-    #     controlplane = false
-    #     ip           = "10.0.0.52"
-    #     cpu          = 12
-    #     memory       = 16
-    #     disk         = 100
-    #     arch         = "amd64"
-    # }
+      {
+        name         = "worker-1"
+        pve_node     = "atuan"
+        controlplane = false
+        ip           = "10.0.0.51"
+        cpu          = 12
+        memory       = 16
+        disk         = 100
+        arch         = "amd64"
+        image        = "https://factory.talos.dev/image/58e4656b31857557c8bad0585e1b2ee53f7446f4218f3fae486aa26d4f6470d8/v1.9.2/nocloud-amd64.raw.zst"
+      },
+      {
+        name         = "worker-2"
+        pve_node     = "atuan"
+        controlplane = false
+        ip           = "10.0.0.52"
+        cpu          = 12
+        memory       = 16
+        disk         = 100
+        arch         = "amd64"
+        image        = "https://factory.talos.dev/image/58e4656b31857557c8bad0585e1b2ee53f7446f4218f3fae486aa26d4f6470d8/v1.9.2/nocloud-amd64.raw.zst"
+    }
   ]
 }
