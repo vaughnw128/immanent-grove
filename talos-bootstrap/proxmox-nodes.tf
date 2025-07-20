7e8c2d479a0e8d26198741e01992a2941abc9171
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2025-07-20 13:25:39 -0400

    Fix pls

--- talos-bootstrap/proxmox-nodes.tf
+++ talos-bootstrap/proxmox-nodes.tf
@@ -8,17 +8,17 @@
 
 locals {
   nodes = [
-      {
-        name         = "controlplane-1"
-        pve_node     = "havnor"
-        controlplane = true
-        ip           = "10.0.0.50"
-        cpu          = 4
-        memory       = 16
-        disk         = 100
-        arch         = "amd64"
-        image        = "https://factory.talos.dev/image/4ec563327034da664de0e0f4b92cfbd68a4eac6700cd5f4f0ab2966eed213469/v1.10.5/nocloud-amd64.raw.zst"
-      },
+      # {
+      #   name         = "controlplane-1"
+      #   pve_node     = "havnor"
+      #   controlplane = true
+      #   ip           = "10.0.0.50"
+      #   cpu          = 4
+      #   memory       = 16
+      #   disk         = 100
+      #   arch         = "amd64"
+      #   image        = "https://factory.talos.dev/image/84f66f3fa52900a0234636ae1da07d5b356cce774673951af35866142158fce6/v1.10.5/nocloud-amd64.raw.zst"
+      # },
       {
         name         = "controlplane-2"
         pve_node     = "selidor"
